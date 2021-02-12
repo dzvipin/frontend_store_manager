03ff0a8a0ab51889681c72c5451398cc65e4671e
dzvipin <78913828+dzvipin@users.noreply.github.com>
2021-02-12 18:41:22 +0530

    update url
    updated url

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   production: false,
-  apiUrl: 'http://3.233.177.86:8083',
+  apiUrl: '',
   geoAPI: 'https://maps.googleapis.com/maps/api/geocode/json',
   createStore:'http://localhost:8802/v1/api/shop',
   getShops:'http://localhost:8802/v1/api/shop',
